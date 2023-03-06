60bfe1352d03461ea380ffe5b3f4c6522386fc33
Amine derbal <aminedoran31@gmail.com>
2023-03-06 19:24:57 +0100

    add button interaction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import './styles/style.css';
-import { getScoresValue } from './modules/score.js';
+import { getGameId, submitNewScore, getAllScores } from './modules/Leaderboard.js';
+import UserInterface from './modules/UserInterface.js';
 
-window.onload = () => {
-  getScoresValue();
+const submitButton = document.getElementById('submit');
+const refreshButton = document.getElementById('refresh');
+const nameInput = document.getElementById('name');
+const scoreInput = document.getElementById('score');
+let gameId = '';
+let ui = '';
+
+window.onload = async () => {
+  gameId = await getGameId();
+  ui = new UserInterface();
 };
+
+refreshButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  const scores = await getAllScores(await gameId);
+  ui.renderScores(await scores.result);
+});
+submitButton.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (nameInput.value === '' || scoreInput.value === '') {
+    return;
+  }
+
+  await submitNewScore(await gameId, nameInput.value, scoreInput.value);
+  ui.clearInputs();
+});
